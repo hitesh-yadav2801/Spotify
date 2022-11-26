5b187ef6897299aebe9075816616af45c899f6b7
Hitesh kumar Yadav <202051086@iiitvadodara.ac.in>
2022-11-26 20:44:18 +0530

    spotify app file

---
+++ app/src/main/java/com/example/spotify/GetStarted.java
@@ -0,0 +1,15 @@
+package com.example.spotify;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class GetStarted extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.get_started);
+    }
+}
