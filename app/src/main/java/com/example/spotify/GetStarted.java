fd3bf3cc70dbe70928973588299f79a46faee7cf
Hitesh kumar Yadav <202051086@iiitvadodara.ac.in>
2022-11-27 10:09:41 +0530

    get started Button added

--- app/src/main/java/com/example/spotify/GetStarted.java
+++ app/src/main/java/com/example/spotify/GetStarted.java
@@ -2,7 +2,10 @@ package com.example.spotify;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class GetStarted extends AppCompatActivity {
 
@@ -11,5 +14,14 @@ public class GetStarted extends AppCompatActivity {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.get_started);
+        Button getStartedBtn = findViewById(R.id.getStartedBtn);
+        getStartedBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent getStartedNext;
+                getStartedNext = new Intent(GetStarted.this,RegisterSignIn.class);
+                startActivity(getStartedNext);
+            }
+        });
     }
 }
--- app/src/main/java/com/example/spotify/SplashScreen.java
+++ app/src/main/java/com/example/spotify/SplashScreen.java
@@ -18,7 +18,7 @@ public class SplashScreen extends AppCompatActivity {
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                Intent iHome = new Intent(SplashScreen.this,MainActivity.class);
+                Intent iHome = new Intent(SplashScreen.this,GetStarted.class);
                 startActivity(iHome);
             }
         },4000);
