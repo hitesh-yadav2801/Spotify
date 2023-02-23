351139165a7ffae6a4116c97f7dfec9b87fc7ee6
Hitesh Yadav <202051086@iiitvadodara.ac.in>
2023-02-23 23:48:53 +0530

    Register activity added

--- app/src/main/java/com/example/spotify/RegisterSignIn.java
+++ app/src/main/java/com/example/spotify/RegisterSignIn.java
@@ -2,7 +2,10 @@ package com.example.spotify;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class RegisterSignIn extends AppCompatActivity {
 
@@ -10,5 +13,14 @@ public class RegisterSignIn extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register_sign_in);
+        Button Register = findViewById(R.id.button2);
+        Register.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View view) {
+                Intent toRegister = new Intent(RegisterSignIn.this, Register.class);
+                startActivity(toRegister);
+            }
+        });
     }
 }
